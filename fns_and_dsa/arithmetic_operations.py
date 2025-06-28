647a0da2a9fc172ae6c3ba952ca836209d7c247e
Mungai7 <mmburu172@gmail.com>
2025-06-29 08:02:37 +0900

    Add files via upload

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,20 @@
+def perform_operation (num1:float,num2:float, operation:str):
+
+    if operation == "+":
+        return (num1 + num2)
+    
+    elif operation == "-":
+        return (num1-num2)
+    
+    elif operation == "/":
+         if num2 == 0 : 
+          raise ValueError("Can not divide by zero")
+         return (num1/num2) 
+    
+    elif operation == "*":
+        return (num1*num2)
+    
+    else:
+        raise ValueError ("Unsupported operation use","+","-","/","*")
+print (perform_operation (10,0,"/"))
+ 
