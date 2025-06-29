f83b95605f0120eef81ca34987416d9860827abf
Mungai7 <mmburu172@gmail.com>
2025-06-29 09:46:28 +0900

    Add files via upload

---
+++ python_introduction/basic_operation.py
@@ -0,0 +1,14 @@
+# basic_operations.py
+number1 = 10
+number2 = 5
+
+#Formulae
+sum_result = number1 + number2
+difference_result = number1 - number2
+product_result = number1 * number2 
+
+#print 
+
+print(f"sum of {number1} and {number2} is {sum_result}")
+print(f"difference of {number1} and {number2} is {difference_result}")
+print(f"product of {number1} and {number2} is {product_result}")
---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,7 @@
+# hours_to_seconds.py
+hours = 2 # 3600 # (60 * 60 seconds)
+second = hours * 3600 # 1 hour = 3600 seconds
+
+
+# print result 
+print(f"{hours} hour(s) is {second} seconds. ")
