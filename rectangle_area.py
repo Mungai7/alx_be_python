9b5070111dcc123d90abb7933c4239ed8355f232
Mungai7 <mmburu172@gmail.com>
2025-07-07 21:35:53 +0300

    Delete rectangle_area.py

--- rectangle_area.py
+++
@@ -1,9 +0,0 @@
-# Define the dimensions of the rectangle
-length = 10   # Length of the rectangle
-width = 5     # Width of the rectangle
-
-# Calculate the area
-area = length * width
-
-# Display the result
-print(f"The area of the rectangle is: {area}")
