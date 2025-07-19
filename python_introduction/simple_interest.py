dd52cb3bc70cb9b49f8c0a5a92f036acd2fb453d
Mungai7 <mmburu172@gmail.com>
2025-07-20 00:18:20 +0300

    weather_advice.txt

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,13 @@
+principal = 1000 # initial investment 
+ 
+rate = 0.05  # annual interest
+
+time = 3  # time in year 
+
+
+# interest.
+interest = principal * rate * time 
+
+# display 
+
+print(f"The simple interest is: {interest}")
